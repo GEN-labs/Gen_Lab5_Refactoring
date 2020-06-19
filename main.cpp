22f12f6444b3b29e58cb86ea73893c62f5ba6597
Maurice <m.lehmann94@gmail.com>
2020-06-19 12:12:15 +0200

    clean ok, ready to submit!

--- main.cpp
+++ main.cpp
@@ -5,27 +5,9 @@
 using namespace std;
 
 /*
+ * Main is commented because of the google tests
 int main() {
-
-    //Should be singleton...
-    std::shared_ptr<Movie::NewReleaseMovie> newReleaseType = std::make_shared<Movie::NewReleaseMovie>();
-    std::shared_ptr<Movie::ChildrensMovie> childrenType = std::make_shared<Movie::ChildrensMovie>();
-
-    std::shared_ptr<Movie> movie1 = std::make_shared<Movie>("Karate Kid");
-    std::shared_ptr<Movie> movie2 = std::make_shared<Movie>("Avengers: Endgame", newReleaseType);
-    std::shared_ptr<Movie> movie3 = std::make_shared<Movie>("Snow White", childrenType);
-
-    Customer customer("Olivier");
-
-    std::shared_ptr<Rental> rental1 = std::make_shared<Rental>(movie1,7);
-    std::shared_ptr<Rental> rental2 = std::make_shared<Rental>(movie2,5);
-    std::shared_ptr<Rental> rental3 = std::make_shared<Rental>(movie3,3);
-
-    customer.addRental(rental1);
-    customer.addRental(rental2);
-    customer.addRental(rental3);
-
-    std::cout << customer.statement() << std::endl;
+    //code here...
 
     return 0;
 }*/
